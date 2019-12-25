9b83a6b16bedc5bcb05a2013c6b16aab42876860
Mark Langovoi <marklangovoi@gmail.com>
2019-12-25 15:20:33 +0300

    ADD A record for krddevdays.ru

--- terraform/docker-swarm-site.tf
+++ terraform/docker-swarm-site.tf
@@ -11,6 +11,14 @@ resource "dnsimple_record" "frontend" {
     ttl = 600
 }
 
+resource "dnsimple_record" "krddevdays" {
+    domain = "krddevdays.ru"
+    name = ""
+    value = "${yandex_compute_instance.docker_swarm.network_interface.0.nat_ip_address}"
+    type = "A"
+    ttl = 600
+}
+
 resource "dnsimple_record" "backend" {
     domain = "${var.domain}"
     name = "backend"
